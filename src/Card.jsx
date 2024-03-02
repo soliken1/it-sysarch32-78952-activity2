87bfbad8851935f183f202b2e67dd01b00660e13
Kenneth James Macas <78952-22232284@SERVER2.DOMAIN>
2024-03-02 19:25:19 +0800

    Update

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,34 +5,29 @@ function Card() {
     return(
         <>
         <div className="card">
-            <img className="image" src={profilePic} alt="pic"></img>
+            <img className="image" src={profilePic}></img>
             <p>Soliken</p>
-            <p>ken@gmail.com</p>
+            <p>soliken@gmail.com</p>
         </div>
         <div className="card">
-            <img className="image" src={profilePic} alt="pic"></img>
-            <p>Soliken</p>
-            <p>ken@gmail.com</p>
+            <img className="image" src={profilePic}></img>
+            <p>LetMeCook</p>
+            <p>letmecook@gmail.com</p>
         </div>
         <div className="card">
-            <img className="image" src={profilePic} alt="pic"></img>
-            <p>Soliken</p>
-            <p>ken@gmail.com</p>
+            <img className="image" src={profilePic}></img>
+            <p>ajwnl</p>
+            <p>ajwnl@gmail.com</p>
         </div>
         <div className="card">
-            <img className="image" src={profilePic} alt="pic"></img>
-            <p>Soliken</p>
-            <p>ken@gmail.com</p>
+            <img className="image" src={profilePic}></img>
+            <p>western union</p>
+            <p>westernunion@gmail.com</p>
         </div>
         <div className="card">
-            <img className="image" src={profilePic} alt="pic"></img>
-            <p>Soliken</p>
-            <p>ken@gmail.com</p>
-        </div>
-        <div className="card">
-            <img className="image" src={profilePic} alt="pic"></img>
-            <p>Soliken</p>
-            <p>ken@gmail.com</p>
+            <img className="image" src={profilePic}></img>
+            <p>ash</p>
+            <p>ash@gmail.com</p>
         </div>
         </>
     ); 
